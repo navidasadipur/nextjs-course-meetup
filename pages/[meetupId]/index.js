496aa42a4d05955ec57a3b053d74974b26ed67a7
navid.asadipur <navid.asadipur@gmail.com>
2022-12-30 13:19:44 +0330

    learn getStaticPaths and how use it for get dynamic id in getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,3 @@
-
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
@@ -10,6 +9,44 @@ function MeetupDetails() {
       description="the meetup description"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "a first meetup",
+        address: "Some Street 5, Some city",
+        description: "the meetup description",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
