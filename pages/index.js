ed0fd1c4e72a3bbd819a9cdf8ee90897b8ca9ce2
navid.asadipur <navid.asadipur@gmail.com>
2022-12-30 13:18:52 +0330

    learn getStaticProps and getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,31 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+  
+//   //fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps(){
+  //fetch data from an Api
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 10
+  };
 }
 
 export default HomePage;
