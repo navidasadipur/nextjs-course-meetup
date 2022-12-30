0b278c8dcc9fb350bc840acd17d5bb1cadfc66b5
navid.asadipur <navid.asadipur@gmail.com>
2022-12-30 10:12:43 +0330

    add MeetupDetail component and return it in MeetupDetails Page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="a first meetup"
+      address="Some Street 5, Some city"
+      description="the meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
